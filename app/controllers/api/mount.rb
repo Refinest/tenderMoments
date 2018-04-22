4b84e7d1285e289dc4eb8c84efa1a070f9fb070c
rennyallen <rennyallen@hotmail.com>
2018-04-22 17:24:54 +0800

    config api documentation

--- app/controllers/api/mount.rb
+++ app/controllers/api/mount.rb
@@ -4,9 +4,18 @@ module API
   class Mount < Grape::API
     format :json
     default_format :json
+    version 'v1', using: :path
 
     mount Diaries
 
-    add_swagger_documentation
+    add_swagger_documentation \
+      base_path: '/api',
+      mount_path: 'doc/swagger',
+      doc_version: '0.0.1',
+      info: {
+        title: "小美好 API 文档",
+        description: "前端：王治文<br />后端：任峻宏",
+      }
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: :registrations }
   root 'home#index'
+  resources :documents, only: :index
 
   mount API::Mount => '/api'
+
 end
